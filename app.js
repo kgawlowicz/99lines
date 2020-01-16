52d9481374f2197934c82e610720e73588eedec9
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-16 15:58:21 -0600

    changed from testing 5 iterations to 99

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let aryFriends = ['Karen','Lori','Dee','Jeff','Joe'];
 for (let ctrFriends = 0; ctrFriends < aryFriends.length; ++ctrFriends) {
     console.log(aryFriends[ctrFriends]);
     //inner loop  ...testing with 5
-    for (let i=5; i>0; i--){
+    for (let i=99; i>0; i--){
         if (i == 1) {
             console.log(i + ' line of code in the file, ' + i + ' line of code; ' + aryFriends[ctrFriends] + 
             ' strikes one out, clears them all out. ' + (i-1) + ' lines of code in the file.');    
